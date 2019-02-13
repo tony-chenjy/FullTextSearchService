2f777dabaea174c65c6e0939cfc0acc0fc5c51b2
yu578707139 <tony.chenjy@foxmail.com>
2019-02-13 11:20:48 +0800

    init

---
+++ src/main/java/org/tony/chenjy/fulltext_search_service/FulltextSearchServiceApplication.java
@@ -0,0 +1,14 @@
+package org.tony.chenjy.fulltext_search_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulltextSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FulltextSearchServiceApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/org/tony/chenjy/fulltext_search_service/FulltextSearchServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package org.tony.chenjy.fulltext_search_service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FulltextSearchServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
